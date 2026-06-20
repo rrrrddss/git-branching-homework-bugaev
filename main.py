3f2c3e1007508c125c9c777c7e77487ef871db85
Bugaev Timofey <rrrrddss@users.noreply.github.com>
2026-06-20 23:47:20 +0300

    feat(profile): подключить вывод профиля в main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-# пример начального main.py
+# main.py после подключения profile.py
+from profile import print_profile
+
 print("Student Branching App")
-print("Проект для тренировки веток Git")
+print_profile()
